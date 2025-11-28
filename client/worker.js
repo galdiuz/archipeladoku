d120e74ee210d0650f37a683d3741ff7ba7cf105
Andreas Lundkvist <andreas@galdiuz.com>
2025-11-28 18:27:01 +0100

    Run worker as an actual worker

---
+++ client/worker.js
@@ -0,0 +1,23 @@
+importScripts('app.js');
+
+const app = Elm.Archipeladoku.Worker.init({
+    flags: {}
+});
+
+self.onmessage = function(event) {
+    console.log(event.data.type, event.data.data);
+
+    switch(event.data.type) {
+        case 'generateBoard':
+            app.ports.receiveGenerateArgs.send(event.data.data);
+
+            break;
+
+        default:
+            console.log('Unknown message type:', event.data.type);
+    }
+};
+
+app.ports.sendBoard.subscribe(function(data) {
+    self.postMessage({ type: 'sendBoard', data: data });
+});

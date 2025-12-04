cdf22110a3c13f35a56233856a8bd12ecb044058
Andreas Lundkvist <andreas@galdiuz.com>
2025-12-04 21:28:18 +0100

    Fix Vite worker config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,8 @@ import elmPlugin from 'vite-plugin-elm';
 
 export default defineConfig({
     plugins: [elmPlugin()],
+    worker: {
+        format: 'es',
+        plugins: () => [elmPlugin()],
+    }
 });

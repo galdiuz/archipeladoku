6e3d602d70ae5655e5163c8b2b0e5a91122f2f99
Andreas Lundkvist <andreas@galdiuz.com>
2025-12-07 13:49:28 +0100

    Add deploy to GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import elmPlugin from 'vite-plugin-elm';
 
 export default defineConfig({
+    base: '/archipeladoku/',
     plugins: [elmPlugin()],
     worker: {
         format: 'es',
